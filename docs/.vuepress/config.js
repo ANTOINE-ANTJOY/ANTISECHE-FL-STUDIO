d448c96578f22cb8773fd51772bb4c2218ad78a5
Etienne Dewailly ∙ ED WEB Créations <etienne@edwebcreations.fr>
2021-02-15 16:37:43 +0100

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: 'Lexique FL Studio - Musique Pro',
+    description: 'Glossaire FL Studio par Musique Pro',
+    dest: 'dist',
+    locales: {
+        '/': {
+            lang: 'fr-FR',
+            title: 'Lexique FL Studio - Musique Pro',
+            serviceWorker: {
+                updatePopup: {
+                    message: 'Une mise à jour est disponible',
+                    buttonText: 'Rafraichir'
+                }
+            },
+        }
+    }
+}
